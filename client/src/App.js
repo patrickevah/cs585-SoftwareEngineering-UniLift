f5c764393417aa2c196936eb866e42dd74cdc6d4
Otunba-B <bryanejiofor@ymail.com>
2022-10-25 01:05:33 -0500

    Changed text in src/app.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This should be our unilift homepage
         </a>
       </header>
     </div>
